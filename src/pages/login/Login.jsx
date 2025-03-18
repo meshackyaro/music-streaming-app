6b568cae6e0af9e60fda72251dd88fd7d52ce326
meshackyaro <yaromeshack@gmail.com>
2025-03-18 09:55:05 +0100

    added login implementation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,43 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import styles from './Login.module.css'
+
 const Login = () => {
-    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+      navigate("/music-list");
+    };
+
+    return (
+      <div className={styles.formContainer}>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button onClick={"/music-list"} type="submit">
+            Login
+          </button>
+        </form>
+        <p>
+          Need an account? <a href="/">Sign Up</a>
+        </p>
+      </div>
+    );
 }
 
 export default Login;
