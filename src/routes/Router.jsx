0fec43f3d4bfed5f3c8d7a9c8fd69174396bcf12
meshackyaro <yaromeshack@gmail.com>
2025-03-18 10:04:47 +0100

    implemented routing for to help navigate between pages

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,6 @@
-import 
+import Layout from "../components/layout/Layout";
+import Login from "../pages/login/Login";
+import MusicList from "../pages/musicList/MusicList";
 
 export const ROUTES = [
     {
