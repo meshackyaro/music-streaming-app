372c21b37802f20c164c9b577456abe8a2a6cc5a
meshackyaro <yaromeshack@gmail.com>
2025-03-18 09:56:58 +0100

    added signup implementation

---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Signup.module.css";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignup = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className={styles.formContainer}>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignup}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <a href="/login">
+          <button onClick={handleSignup} type="submit">
+            Sign Up
+          </button>
+        </a>
+      </form>
+      <p>
+        Already have an account? <a href="/login">Login</a>
+      </p>
+    </div>
+  );
+};
+
+export default Signup;
